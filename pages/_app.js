3f013524036e535c75c43a8a6393f8dee72c95cd
vivek kumar <vivekr4400@gmail.com>
2024-04-20 18:25:34 +0530

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import Script from "next/script";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
